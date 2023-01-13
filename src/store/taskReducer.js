4e07d65037c91bff11c1de771b8b8d157ed70f91
Dmitry <neocd@me.com>
2023-01-13 15:05:49 +0300

    Homework_1

---
+++ src/store/taskReducer.js
@@ -0,0 +1,24 @@
+import { taskUpdated } from "./actionTypes";
+import { taskDeleted } from "./actionTypes";
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    // case "task/completed":
+    //   const newArray = [...state];
+    //   const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+    //   newArray[elementIndex].completed = true;
+    //   return newArray;
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+      newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
+      return newArray;
+    }
+    case taskDeleted: {
+      return state.filter((el) => el.id !== action.payload.id);
+    }
+
+    default:
+      return state;
+  }
+}
